349f4125e97b268a9e2ce863b5593344c111cc14
hai14894 <hohoanghai14894@gmail.com>
2020-09-23 22:19:40 +1000

    render out todos from API call

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,31 +1,33 @@
 import axios from "axios";
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: "Task 1"
-        },
-        {
-            id: 2,
-            title: "Task 2"
-        }
-    ]
+  todos: [
+    {
+      id: 1,
+      title: "Task 1",
+    },
+    {
+      id: 2,
+      title: "Task 2",
+    },
+  ],
 };
 const getters = {
-    allTodos: (state) => state.todos
+  allTodos: (state) => state.todos,
 };
 const actions = {
-    async fetchTodos() {
-        const res  = await axios.get('https://jsonplaceholder.typicode.com/todos')
-        console.log(res.data);
-    }
+  async fetchTodos({ commit }) {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    commit("setTodos", res.data);
+  },
 };
-const mutations = {};
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+};
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations
-}
+  state,
+  getters,
+  actions,
+  mutations,
+};
